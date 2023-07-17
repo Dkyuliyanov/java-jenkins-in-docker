a480dd3c7538c0b187b99bf202503d6fba3d2de5
dkarailiev-eti <dian.karailiev@etisoftware.com>
2023-07-17 12:27:46 +0300

    changed target to java11

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println(checkIfInputIsAnEvenNumber(122));
+
+    System.out.println("This is a test change for build after commit.");
   }
 
   public static boolean checkIfInputIsAnEvenNumber(int number) {
